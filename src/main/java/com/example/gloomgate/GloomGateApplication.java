92c8cecea335129549d1df6eb71b20bce3ad1fde
Auke Aardoom <keauk8@gmail.com>
2023-09-03 13:25:29 +0200

    Initial commit.

---
+++ src/main/java/com/example/gloomgate/GloomGateApplication.java
@@ -0,0 +1,13 @@
+package com.example.gloomgate2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GloomGateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GloomGateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gloomgate/GloomGate2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gloomgate2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GloomGate2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
